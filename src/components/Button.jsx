11f3b8f35092e0008ae1460c798e4db3247aef14
joshita689-svg <joshita689@gmail.com>
2026-06-24 19:36:46 -0400

    Starting Over

--- src/components/Button.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function Button({ children, variant = 'primary', className = '', ...props }) {
-  return (
-    <button className={`button button-${variant} ${className}`} {...props}>
-      {children}
-    </button>
-  );
-}
--- src/components/SymptomButton.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function SymptomButton({ label, active, onClick }) {
-  return (
-    <button className={`symptom-button ${active ? 'active' : ''}`} onClick={onClick}>
-      <span>{label}</span>
-    </button>
-  );
-}
